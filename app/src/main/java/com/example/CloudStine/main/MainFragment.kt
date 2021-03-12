0f4434a88da839b6eb2dcb3a557e4f302fd8a611
Jan Scheiwe <jan.scheiwe@bornholdtlee.de>
2021-03-12 11:05:00 +0100

    fix feet display

--- app/src/main/java/com/example/CloudStine/main/MainFragment.kt
+++ app/src/main/java/com/example/CloudStine/main/MainFragment.kt
@@ -1,14 +1,21 @@
 package com.example.CloudStine.main
 
+import android.Manifest
+import android.content.pm.PackageManager
+import android.location.Location
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.example.CloudStine.R
 import com.example.CloudStine.databinding.MainFragmentBinding
 import com.google.android.material.snackbar.Snackbar
+import java.lang.StringBuilder
+import kotlin.math.roundToInt
 
 class MainFragment : Fragment(R.layout.main_fragment) {
 
@@ -40,25 +47,31 @@ class MainFragment : Fragment(R.layout.main_fragment) {
     }
 
     private fun setObservers() {
+        mainViewModel.status.observe(viewLifecycleOwner) { message -> showStatus(message) }
+
+        mainViewModel.cloudOpacity.observe(viewLifecycleOwner) { opacity -> binding.cloudOpacityData.text = opacity }
+        mainViewModel.cloudVisibility.observe(viewLifecycleOwner) { visibility -> binding.cloudVisibilityData.text = visibility }
         mainViewModel.cloudHeight.observe(viewLifecycleOwner) { height ->
             binding.cloudHeightDataMeter.text = height
-//            val convertedHeight = convertHeight(height)
-//            binding.cloudHeightDataFeet.text = (convertedHeight * 3.2808f).toString()
-        }
-        mainViewModel.cloudVisibility.observe(viewLifecycleOwner) { visibility -> binding.cloudVisibilityData.text = visibility}
-        mainViewModel.status.observe(viewLifecycleOwner) { message ->
-            binding.swiperefreshMain.isRefreshing = false
-            showStatusSnackBar(message)
+            binding.cloudHeightDataFeet.text = convertHeight(height)
         }
     }
 
-    private fun convertHeight(height: String): Float {
+    private fun convertHeight(height: String): String {
         val noMHeight = (height.substring(0, height.indexOf("m") - 1))
-        val doublePoint = noMHeight.replace(",", ".")
-        return doublePoint.replaceFirst(".", ",").toFloat()
+        val germanWithoutFirst = noMHeight.replace(".", "")
+        val englishComma = germanWithoutFirst.replace(",", ".")
+        val floatHeight = englishComma.toFloat()
+        val heightInFeet = floatHeight * 3.2808f
+        val roundedHeight = heightInFeet.roundToInt()
+        val feetString = roundedHeight.toString()
+        val stringWithDot = StringBuilder(feetString).insert(feetString.length - 3, ".")
+        val stringWithFt = StringBuilder(stringWithDot).append(" ft")
+        return stringWithFt.toString()
     }
 
-    private fun showStatusSnackBar(message: String) {
+    private fun showStatus(message: String) {
+        binding.swiperefreshMain.isRefreshing = false
         Snackbar.make(binding.root, message, Snackbar.LENGTH_SHORT).show()
     }
 
--- app/src/main/java/com/example/CloudStine/main/MainViewModel.kt
+++ app/src/main/java/com/example/CloudStine/main/MainViewModel.kt
@@ -9,8 +9,12 @@ import kotlinx.coroutines.launch
 
 class MainViewModel : ViewModel() {
 
+    private val START_OPACITY = "Bewölkung"
     private val START_HEIGHT = "Wolkenhöhe"
     private val START_VISIBILITY = "Sichtweite"
+
+    private val _cloudOpacity = MutableLiveData<String>()
+    val cloudOpacity: LiveData<String> = _cloudOpacity
 
     private val _cloudHeight = MutableLiveData<String>()
     val cloudHeight: LiveData<String> = _cloudHeight
@@ -29,15 +33,19 @@ class MainViewModel : ViewModel() {
             try {
                 val allData = cloudRepository.getData().body()!!.string()
 
+                val opacityIndex = allData.indexOf(START_OPACITY) + 30
                 val heightIndex = allData.indexOf(START_HEIGHT) + 32
                 val visibilityIndex = allData.indexOf(START_VISIBILITY) + 31
 
+                val opacityEndIndex = allData.indexOf("<", opacityIndex)
                 val heightEndIndex = allData.indexOf("<", heightIndex)
                 val visibilityEndIndex = allData.indexOf("<", visibilityIndex)
 
+                val cutOpacityData = allData.substring(opacityIndex, opacityEndIndex)
                 val cutHeightData = allData.substring(heightIndex, heightEndIndex)
                 val cutVisibilityData = allData.substring(visibilityIndex, visibilityEndIndex)
 
+                _cloudOpacity.value = cutOpacityData
                 _cloudHeight.value = cutHeightData
                 _cloudVisibility.value = cutVisibilityData
                 _status.value = "Data was retrieved"
